d57987434546e1bcc6e5e0f087136642725a821a
Gurthog <226201902+Gurthog@users.noreply.github.com>
2025-10-11 00:03:52 -0700

    add score text

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import pygame
 
-from constants import (
-    SCREEN_HEIGHT,
-    SCREEN_WIDTH,
-)
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from player import Player
 from shot import Shot
+from constants import (
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    FONT_COLOR,
+)
 
 
 def main():
     pygame.init()
+    pygame.font.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
@@ -28,6 +30,10 @@ def main():
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    score = 0
+    score_font = pygame.font.SysFont("Comic Sans MS", 44)
+    score_text = score_font.render(f"Score: {score}", True, FONT_COLOR)
+
     dt = 0
     running = True
     while running:
@@ -41,16 +47,20 @@ def main():
         # resolve
         for rock in asteroids:
             if rock.detect_collision(player):
-                print("Game over!")
+                print(f"Game over! Score: {score}")
                 running = False
 
             for shot in shots:
                 if rock.detect_collision(shot):
                     shot.kill()
                     rock.split()
+                    score += 1
+                    score_text = score_font.render(
+                        f"Score: {score}", False, FONT_COLOR)
 
         # draw
         screen.fill("black")
+        screen.blit(score_text, (10, 10))
         for sprite in drawable:
             sprite.draw(screen)
 
